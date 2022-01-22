0d65e2dec6efde519bb26687efd40b6497b13cc5
Anne Stein <anne.stein.dm@gmail.com>
2022-01-22 13:02:56 +0100

    minimal working example of spring shell

--- src/main/kotlin/com/nuwe/challenge/computerwoman/ComputerwomanApplication.kt
+++ src/main/kotlin/com/nuwe/challenge/computerwoman/ComputerwomanApplication.kt
@@ -1,16 +1,10 @@
 package com.nuwe.challenge.computerwoman
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ComputerwomanApplication : CommandLineRunner {
-	override fun run(vararg args: String?) {
-		TODO("Not yet implemented")
-	}
-
+class ComputerwomanApplication
 	fun main(args: Array<String>) {
 		runApplication<ComputerwomanApplication>(*args)
 	}
-}
