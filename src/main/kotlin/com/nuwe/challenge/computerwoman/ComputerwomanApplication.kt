2d3561762d70a2646fed0897deb19726eb33294e
Anne Stein <anne.stein.dm@gmail.com>
2022-01-22 10:56:34 +0100

    starter commit

---
+++ src/main/kotlin/com/nuwe/challenge/computerwoman/ComputerwomanApplication.kt
@@ -0,0 +1,16 @@
+package com.nuwe.challenge.computerwoman
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComputerwomanApplication : CommandLineRunner {
+	override fun run(vararg args: String?) {
+		TODO("Not yet implemented")
+	}
+
+	fun main(args: Array<String>) {
+		runApplication<ComputerwomanApplication>(*args)
+	}
+}
---
+++ src/test/kotlin/com/nuwe/challenge/computerwoman/ComputerwomanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nuwe.challenge.computerwoman
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComputerwomanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
